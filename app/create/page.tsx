95d090eb1462bd82e24a0b2da08b3082eccc0178
Hengers3012 <hengersrosario.m@gmail.com>
2023-07-22 00:45:16 -0400

    feat: si existen modelos y años de marca este muestra -> modelos -> y los años de cada modelo | | si existen modelos pero estos no tienen años, solo se muestran los modelos de esa marca || si la marca no tiene ningun modelo, no me muestra nada...

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,51 +1,105 @@
+/* eslint-disable multiline-ternary */
 /* eslint-disable react/jsx-key */
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import jsonData from "./createdata.json";
+"use client";
+import React, { ChangeEvent, useState } from "react";
+import json from "./createdata.json";
 
-console.log(jsonData);
+const CarsList: React.FC = () => {
+  const [selectedCar, setSelectedCar] = useState("");
+  const [selectedModel, setSelectedModel] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
 
-interface MyData {
-  id: number;
-  name: string;
-  logo: string;
-}
+  const handleCarSelection = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCar(e.target.value);
+    setSelectedModel("");
+    setSelectedYear("");
+  };
 
-// Lee el archivo JSON
+  const handleModelSelection = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedModel(e.target.value);
+    setSelectedYear("");
+  };
 
-function Create_Cars() {
-  //   const [selectedName, setSelectedName] = useState<string>("");
-  //   const [selectedObject, setSelectedObject] = useState<MyData | null>(null);
-  //   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //     const name = e.target.value;
-  //     const selectedObj = data.find((item) => item.name === name);
-  //     setSelectedObject(selectedObj || null);
-  //   };
+  const handleYearSelection = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(e.target.value);
+  };
+
+  const selectedData = json.find((item: any) => item.name === selectedCar);
+
+  const selectedModelData = selectedData?.models?.find(
+    (model) => model.model === selectedModel
+  );
+
   return (
-    <form className="bg-white p-4 mt-16 min-w-full justify-center overflow-hidden">
+    <div className="bg-white p-4 mt-16 min-w-full justify-center overflow-hidden">
       <h1 className="sm:text-3xl  text-2xl font-medium title-font mb-4 text-gray-900">
         Añade un nuevo vehiculo
       </h1>
+
       <select
-        name="HeadlineAct"
-        id="HeadlineAct"
-        className="mt-1.5 w-52 h-8 rounded-lg pl-2 border-gray-300 text-gray-700 sm:text-sm"
+        className="mt-1.5 w-fit h-8 rounded-lg pl-2 border-gray-300 text-gray-700 sm:text-sm"
+        value={selectedCar}
+        onChange={handleCarSelection}
       >
-        {data.map(({ id, name, logo }) => (
-          <option key={id}>{name}</option>
+        <option className="m-1" value="">
+          Seleccione una marca de automóvil
+        </option>
+        {json.map((car) => (
+          <option key={car.id} value={car.name}>
+            {car.name}
+          </option>
         ))}
       </select>
+      <select
+        className="mt-1.5 w-fit h-8 rounded-lg pl-2 border-gray-300 text-gray-700 sm:text-sm"
+        disabled={!selectedData?.models}
+        value={selectedModel}
+        onChange={handleModelSelection}
+      >
+        <option className="m-1" value="">
+          Seleccione un modelo
+        </option>
+        {selectedData?.models &&
+          selectedData.models.map((model) => (
+            <option key={model.model} value={model.model}>
+              {model.model}
+            </option>
+          ))}
+      </select>
+      <select
+        className="mt-1.5 w-fit h-8 rounded-lg pl-2 border-gray-300 text-gray-700 sm:text-sm"
+        disabled={!selectedModelData?.años}
+        value={selectedYear}
+        onChange={handleYearSelection}
+      >
+        <option className="m-1" value="">
+          Seleccione un año
+        </option>
+        {selectedModelData &&
+          selectedModelData.años &&
+          selectedModelData.años.map((año: any) => (
+            <option className="m-1" key={año} value={año}>
+              {año}
+            </option>
+          ))}
+      </select>
 
-      <div className="flex flex-wrap my-2 mx-2 justify-normal items-center ">
-        {data.map(({ id, name, logo }) => (
-          <div className="flex flex-col w-16 h-18 mx-10 my-4 items-center">
-            <img src={logo} alt={name} className="w-16 h-14 " />
-            <h3 className="text-gray-900 tet">{name}</h3>
-          </div>
-        ))}
+      <div className="flex flex-col my-2 mx-2 justify-normal items-center ">
+        <h1 className="sm:text-3xl  text-2xl font-medium title-font mb-4 text-gray-900">
+          Todas las Marcas
+        </h1>
+
+        <div className="flex flex-wrap my-2 mx-2 justify-normal items-center ">
+          {json.map(({ id, name, logo }) => (
+            <div className="flex flex-col w-40 h-20 mx-10 my-4 items-center justify-evenly">
+              <img src={logo} alt={name} className="w-16 h-20 " />
+              <h3 className="text-gray-900 tet">{name}</h3>
+            </div>
+          ))}
+        </div>
       </div>
-    </form>
+    </div>
   );
-}
+};
 
-export default Create_Cars;
+export default CarsList;
